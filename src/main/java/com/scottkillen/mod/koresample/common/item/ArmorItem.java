3b70c30c5436883f0c2051b3cc5b24c59626d45b
Scott Killen <scott@scottkillen.com>
2015-09-20 00:28:35 -0400

    Remove unneeded qualification

--- src/main/java/com/scottkillen/mod/koresample/common/item/ArmorItem.java
+++ src/main/java/com/scottkillen/mod/koresample/common/item/ArmorItem.java
@@ -6,7 +6,7 @@ import javax.annotation.Nonnull;
 @SuppressWarnings({ "WeakerAccess", "AbstractClassNeverImplemented", "ClassHasNoToStringMethod" })
 public abstract class ArmorItem extends ItemArmor
 {
-    protected ArmorItem(ItemArmor.ArmorMaterial armorMaterial, int renderIndex, @Nonnull ArmorType armorType,
+    protected ArmorItem(ArmorMaterial armorMaterial, int renderIndex, @Nonnull ArmorType armorType,
                         @Nonnull String iconPath)
     {
         super(armorMaterial, renderIndex, armorType.value());

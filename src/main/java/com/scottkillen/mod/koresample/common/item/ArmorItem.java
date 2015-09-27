2ec69d4d3c91dbf8c004abf21b2e115d1450c7b9
Scott Killen <scott@scottkillen.com>
2015-09-27 17:49:42 -0400

    Add sanity checks.

--- src/main/java/com/scottkillen/mod/koresample/common/item/ArmorItem.java
+++ src/main/java/com/scottkillen/mod/koresample/common/item/ArmorItem.java
@@ -1,15 +1,17 @@
 package com.scottkillen.mod.koresample.common.item;
 
+import com.google.common.base.Preconditions;
 import net.minecraft.item.ItemArmor;
 import javax.annotation.Nonnull;
 
 @SuppressWarnings({ "WeakerAccess", "AbstractClassNeverImplemented" })
 public abstract class ArmorItem extends ItemArmor
 {
-    protected ArmorItem(ArmorMaterial armorMaterial, int renderIndex, @Nonnull ArmorType armorType,
+    protected ArmorItem(@Nonnull ArmorMaterial armorMaterial, int renderIndex, @Nonnull ArmorType armorType,
                         @Nonnull String iconPath)
     {
         super(armorMaterial, renderIndex, armorType.value());
+        Preconditions.checkArgument(!iconPath.isEmpty());
         setTextureName(iconPath);
     }
 }
